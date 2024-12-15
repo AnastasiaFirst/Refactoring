e48d95d874d64190a4db2d908d1c1dea0175553c
AnastasiaFirst <92177620+AnastasiaFirst@users.noreply.github.com>
2024-12-15 18:19:39 +0300

    Update Server.java

--- Refactoring/src/main/java/Server.java
+++ Refactoring/src/main/java/Server.java
@@ -1,25 +1,25 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    private final int port;
-    private final List<String> validPaths;
     private final ExecutorService threadPool;
+    private final Map<String, Map<String, Handler>> handlers;
 
-    public Server(int port, int poolSize, List<String> validPaths) {
-        this.port = port;
-        this.validPaths = validPaths;
+    public Server(int poolSize) {
         this.threadPool = Executors.newFixedThreadPool(poolSize);
+        this.handlers = new ConcurrentHashMap<>();
     }
 
-    public void start() {
+    public void addHandler(String method, String path, Handler handler) {
+        handlers.computeIfAbsent(method, k -> new ConcurrentHashMap<>()).put(path, handler);
+    }
+
+    public void listen(int port) {
         try (final var serverSocket = new ServerSocket(port)) {
             System.out.println("Server is running on port " + port);
             while (true) {
@@ -38,47 +38,33 @@ public class Server {
                 final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 final var out = new BufferedOutputStream(socket.getOutputStream());
         ) {
+
             final var requestLine = in.readLine();
             final var parts = requestLine.split(" ");
-
             if (parts.length != 3) {
                 return;
             }
 
+            final var method = parts[0];
             final var path = parts[1];
-            if (!validPaths.contains(path)) {
+
+            String line;
+            while (!(line = in.readLine()).isEmpty()) {
+            }
+
+            BufferedReader body = in;
+            Request request = new Request(method, path, body);
+
+            Handler handler = findHandler(method, request.getPath());
+            if (handler != null) {
+                handler.handle(request, out);
+            } else {
                 out.write(("HTTP/1.1 404 Not Found\r\n" +
                         "Content-Length: 0\r\n" +
                         "Connection: close\r\n" +
                         "\r\n").getBytes());
                 out.flush();
-                return;
             }
-
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
-
-            if (path.equals("/classic.html")) {
-                final var template = Files.readString(filePath);
-                final var content = template.replace("{time}", LocalDateTime.now().toString()).getBytes();
-                out.write(("HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + content.length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n").getBytes());
-                out.write(content);
-                out.flush();
-                return;
-            }
-
-            final var length = Files.size(filePath);
-            out.write(("HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: " + mimeType + "\r\n" +
-                    "Content-Length: " + length + "\r\n" +
-                    "Connection: close\r\n" +
-                    "\r\n").getBytes());
-            Files.copy(filePath, out);
-            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -88,5 +74,13 @@ public class Server {
                 e.printStackTrace();
             }
         }
+    }
+
+    private Handler findHandler(String method, String path) {
+        Map<String, Handler> methodHandlers = handlers.get(method);
+        if (methodHandlers != null) {
+            return methodHandlers.get(path);
+        }
+        return null;
     }
 }
